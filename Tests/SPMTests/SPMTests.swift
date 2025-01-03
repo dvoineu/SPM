4087729d73881694b212f2e16c98e6c3975df7c3
Dvoineu Aliaksandr <av.dvoinev@gmail.com>
2025-01-03 18:52:12 +0300

    Documented and updated first public API for color

--- Tests/SPMTests/SPMTests.swift
+++ Tests/SPMTests/SPMTests.swift
@@ -4,3 +4,9 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
+
+
+@Test func testColorRedEqual() {
+    let color = SPM.colorFromHexString("FF0000")
+    #expect(color == .red)
+}
