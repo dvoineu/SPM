e10382a73d1c659a5d563f3a9dbe352f9d9abee8
Dvoineu Aliaksandr <av.dvoinev@gmail.com>
2025-01-03 16:18:15 +0300

    add chages in Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SPM",
+            type: .dynamic,
             targets: ["SPM"]),
     ],
     targets: [
